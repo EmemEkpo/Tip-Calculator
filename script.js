0f6107a855780337677df61ea5db879c3eb09fa0
EmemEkpo <88948036+EmemEkpo@users.noreply.github.com>
2022-09-23 15:42:19 +0100

    modified:   script.js

--- script.js
+++ script.js
@@ -4,32 +4,31 @@ let totalElem = document.getElementById("totalEl");
 let resetElem = document.getElementById("reset");
 
 const percent5 = document.getElementById("percent5");
-percent5.addEventListener("input", () => {
+percent5.addEventListener("click", () => {
   addPercent(5);
 });
 const percent10 = document.getElementById("percent10");
-percent10.addEventListener("input", () => {
+percent10.addEventListener("click", () => {
   addPercent(10);
 });
 const percent15 = document.getElementById("percent15");
-percent15.addEventListener("input", () => {
+percent15.addEventListener("click", () => {
   addPercent(15);
 });
 const percent25 = document.getElementById("percent25");
-percent25.addEventListener("input", () => {
+percent25.addEventListener("click", () => {
   addPercent(25);
 });
 const percent50 = document.getElementById("percent50");
-percent50.addEventListener("input", () => {
+percent50.addEventListener("click", () => {
   addPercent(50);
 });
 const percent100 = document.getElementById("percent100");
-percent100.addEventListener("input", () => {
+percent100.addEventListener("click", () => {
   addPercent(100);
 });
 let tip = 0;
 let total = 0;
-let bill = 0;
 function addPercent(percent) {
   const bill = parseInt(billElem.value);
   const people = parseInt(peopleElem.value);
@@ -40,6 +39,7 @@ function addPercent(percent) {
 function UpdateUI() {
   tipElem.innerText = `$${tip}`;
   totalElem.innerText = `$${total}`;
+  console.log();
 }
 function reset() {
   billElem.value = "";
